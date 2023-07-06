ffaefadcb94add2cf63c7698b441b117ec4ae90a
elecun <elecun2@gmail.com>
2023-07-07 08:03:22 +0900

    fixed wrong url

--- app_nback_visual/urls.py
+++ app_nback_visual/urls.py
@@ -5,7 +5,7 @@ from app_nback_visual import views
 
 urlpatterns = [
     path('', views.index, name="app_nback_visual_index"),
-    path('/2back', views.back2, name="app_nback_visual_2back"),
+    path('2back/', views.back2, name="app_nback_visual_2back"),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
