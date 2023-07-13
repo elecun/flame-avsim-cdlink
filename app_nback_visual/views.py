85f17d6bd6f2f528ccddfd31b75fe7cec8072ed9
elecun <elecun2@gmail.com>
2023-07-13 19:55:41 +0900

    update resources

--- app_nback_visual/views.py
+++ app_nback_visual/views.py
@@ -9,8 +9,6 @@ def index_2back(request):
     return render(request, "html/index_2back.html")
 def page_2back(request):
     return render(request, "html/code_page.html")
-def page(request, code):
-    return render(request, "html/code_page.html", context={'code':code})
 
 # 3-back task index page
 def index_3back(request):
